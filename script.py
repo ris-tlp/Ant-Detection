01a1e04430a33ae62276d0dc0f7e673701666576
Omar Pervez <omarkhantlp@gmail.com>
2018-08-03 20:24:23 +0300

    removed unnecessary import

--- script.py
+++ script.py
@@ -1,19 +1,17 @@
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 ant_cascade = cv2.CascadeClassifier('cascade.xml')
 
 img = cv2.imread('sample.jpeg')
 cv2.imshow("undetected", img)
 
-
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ants = ant_cascade.detectMultiScale(img,1.1,1)
+
 for (x,y,w,h) in ants:
     cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)
-
 
 cv2.imshow('detected',img)
 cv2.waitKey()
